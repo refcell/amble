789d98602d3f1bdc82823b68c7058488eabbb733
refcell <abigger87@gmail.com>
2023-10-27 14:36:22 -0400

    fix amble include str

--- bin/amble/src/lib.rs
+++ bin/amble/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../../README.md")]
+#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/refcell/amble/main/etc/logo.png",
     html_favicon_url = "https://raw.githubusercontent.com/refcell/amble/main/etc/favicon.ico",
